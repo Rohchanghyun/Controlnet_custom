454039c95836178ec7e4cc0220f492e04976a3ae
JJanghyun97 <ggara376@naver.com>
2024-12-02 12:49:12 +0000

    12/02 busan upload

---
+++ local_adapter/pose_test.py
@@ -0,0 +1,52 @@
+from PIL import Image
+import torch
+from diffusers import ControlNetModel, StableDiffusionControlNetPipeline
+from diffusers import EulerAncestralDiscreteScheduler, PNDMScheduler
+
+def generate_image_from_condition(input_image_path, prompt, negative_prompt=""):
+    # ControlNet 모델 로드
+    controlnet = ControlNetModel.from_pretrained(
+        "lllyasviel/sd-controlnet-openpose",
+        torch_dtype=torch.float16
+    )
+
+    # Stable Diffusion 파이프라인 설정
+    pipe = StableDiffusionControlNetPipeline.from_pretrained(
+        "runwayml/stable-diffusion-v1-5",
+        controlnet=controlnet,
+        torch_dtype=torch.float16
+    )
+    
+    # GPU 사용 설정
+    pipe.to("cuda")
+    
+    # Euler Ancestral sampler 설정
+    #pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
+    pipe.scheduler = PNDMScheduler.from_config(pipe.scheduler.config)
+    
+    
+    # 입력 이미지 로드 및 전처리 (768x768로 변경)
+    input_image = Image.open(input_image_path)
+    input_image = input_image.resize((512, 512))
+    
+    # 이미지 생성
+    output_image = pipe(
+        prompt=prompt,
+        negative_prompt=negative_prompt,
+        image=input_image,
+        num_inference_steps=30,
+        guidance_scale=7.5,
+        height=512,
+        width=512
+    ).images[0]
+    
+    return output_image
+
+# 사용 예시
+if __name__ == "__main__":
+    input_image_path = "/workspace/data/changhyun/dataset/oddoong/test.png"
+    prompt = "a man crying"
+    negative_prompt = "low quality, blurry, distorted, bad anatomy, ugly, poor details"
+    
+    generated_image = generate_image_from_condition(input_image_path, prompt, negative_prompt)
+    generated_image.save("generated_output.png")
